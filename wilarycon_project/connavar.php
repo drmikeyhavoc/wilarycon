9f99d73fcfb3cbd24d4e7b080ccb9ddaa34f7fbd
drmikeyhavoc <mikeyhavoc@hotmail.com>
2014-06-26 21:42:56 +1200

    Update connavar.php
    Added paragraph to connavar.php

--- wilarycon_project/connavar.php
+++ wilarycon_project/connavar.php
@@ -3,7 +3,10 @@
 	<head><title>Connavar's Page</title></head>
 	<body>
 		<h1>Welcome to Connavar's Page</h1>
+		
 		<div>Connavar is my only boy and heir to the throne</div>
+		
+		<p>This will be my first paragraph</p>
 		
 	</body>
 </html>
