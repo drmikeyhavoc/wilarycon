069f51f8b2874fdb96025a253d313bc4f8d44fa1
mikeyhavoc <mikeyhavoc@hotmail.com>
2014-06-26 21:40:20 +1200

    Added connavar.php to the project

---
+++ wilarycon_project/connavar.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head><title>Connavar's Page</title></head>
+	<body>
+		<h1>Welcome to Connavar's Page</h1>
+		<div>Connavar is my only boy and heir to the throne</div>
+		
+	</body>
+</html>
