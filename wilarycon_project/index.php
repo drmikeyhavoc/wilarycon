2611fe80b3e13f28b04490a74dde3c7222882e47
mikeyhavoc <mikeyhavoc@hotmail.com>
2014-06-26 17:11:37 +1200

    Added index.php

---
+++ wilarycon_project/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html!>
+<html>
+	<head>
+		<title>Wilarycon Homepage</title>
+	</head>
+	<body>
+		<h1>Welcome to Willow, Arya and Connavars Homepage</h1>
+		
+		
+	</body>
+</html>
+
+
+
+<?php
+
+?>
